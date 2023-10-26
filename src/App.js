2b59b541602c0274942197c909eb10e817559361
tekineta <111514255+tekineta@users.noreply.github.com>
2023-10-26 10:29:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Screens/Home/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+
+
+export default App;
---
+++ src/screens/home.js
@@ -0,0 +1,9 @@
+import { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+
+
+
+export default home
